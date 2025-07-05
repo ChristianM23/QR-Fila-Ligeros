c4a535f08df86e7fc3920786f657d47d9b8d5e54
Christian <christian.mico23@gmail.com>
2025-07-05 12:30:54 +0200

    delete old files

--- old/config/config.php
+++
@@ -1,4 +0,0 @@
-<?php 
-//$IP = "https://pipopu.es/api-qr/";
-$IP = "https://localhost/api-qr/";
-?>
--- old/config/db.php
+++
@@ -1,5 +0,0 @@
-<?php
-//$db = new mysqli("qaix219.pipopu.es", "qaix219", "CHpipopu.23", "qaix219");
-$db = new mysqli("localhost", "root", "", "filaqr");
-mysqli_set_charset($db, 'utf8');
-?>
